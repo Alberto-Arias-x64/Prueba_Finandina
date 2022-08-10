36e12eebb3af1663b89ff994263a6afa17ab01af
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-08-10 18:22:32 -0500

    preview

--- main.js
+++ main.js
@@ -1,32 +1,82 @@
+let Team = false
 const Change = () => {
-    // Change Text button
-    const Text = document.querySelector('button')
-    Text.innerHTML='CAMBIAR A CREDITO'
 
-    // Change Color Schema
-    const Root_Element = document.querySelector(':root')
-    Root_Element.style.setProperty('--principal_gradient_90', 'linear-gradient(90deg, #F7276C, #A703F6)')
-    Root_Element.style.setProperty('--principal_gradient_45', 'linear-gradient(135deg, #F7276C, #A703F6)')
-    Root_Element.style.setProperty('--principal_color', '#A703F6')
+    const Change_To_Red = () => {
+        // Change Text button
+        const Text = document.querySelector('button')
+        Text.innerHTML = 'CAMBIAR A CREDITO'
 
-    // Change Images
-    const Image = document.querySelector('#phone_img')
-    Image.src = './assets/Top/Phone_Red.svg'
+        // Change Color Schema
+        const Root_Element = document.querySelector(':root')
+        Root_Element.style.setProperty('--principal_gradient_90', 'linear-gradient(90deg, #A703F6, #F7276C )')
+        Root_Element.style.setProperty('--principal_gradient_45', 'linear-gradient(135deg, #A703F6, #F7276C )')
+        Root_Element.style.setProperty('--principal_color', '#A703F6')
 
-    // Hide Elements
-    const Team_Blue = document.querySelectorAll('.team_blue')
-    const Node_List_Blue = Array.from(Team_Blue)
-    Node_List_Blue.map(element => {
-        console.log(element)
-        element.classList.add('hide')
-    })
+        // Change Images
+        const Image = document.querySelector('#phone_img')
+        Image.src = './assets/Top/Phone_Red.svg'
 
-    // Show Elements
-    const Team_Red = document.querySelectorAll('.team_red')
-    const Node_List_Red = Array.from(Team_Red)
-    Node_List_Red.map(element => {
-        console.log(element)
-        element.classList.remove('hide')
-    })
+        // Hide Elements
+        const Team_Blue = document.querySelectorAll('.team_blue')
+        const Node_List_Blue = Array.from(Team_Blue)
+        Node_List_Blue.map(element => {
+            element.classList.add('hide')
+        })
 
+        // Show Elements
+        const Team_Red = document.querySelectorAll('.team_red')
+        const Node_List_Red = Array.from(Team_Red)
+        Node_List_Red.map(element => {
+            element.classList.remove('hide')
+        })
+
+        //Change Elements
+        const Dynamic_Title = document.querySelector('#dynamic_title')
+        Dynamic_Title.innerHTML = 'Compra de cartera'
+    }
+
+    const Change_To_Blue = () => {
+        // Change Text button
+        const Text = document.querySelector('button')
+        Text.innerHTML = 'CAMBIAR A TARJETA'
+
+        // Change Color Schema
+        const Root_Element = document.querySelector(':root')
+        Root_Element.style.setProperty('--principal_gradient_90', 'linear-gradient(90deg, #048FF7, #07FAF2 )')
+        Root_Element.style.setProperty('--principal_gradient_45', 'linear-gradient(135deg, #048FF7, #07FAF2)')
+        Root_Element.style.setProperty('--principal_color', '#07FAF2')
+
+        // Change Images
+        const Image = document.querySelector('#phone_img')
+        Image.src = './assets/Top/Phone_Blue.svg'
+
+        // Hide Elements
+        const Team_Blue = document.querySelectorAll('.team_red')
+        const Node_List_Blue = Array.from(Team_Blue)
+        Node_List_Blue.map(element => {
+            element.classList.add('hide')
+        })
+
+        // Show Elements
+        const Team_Red = document.querySelectorAll('.team_blue')
+        const Node_List_Red = Array.from(Team_Red)
+        Node_List_Red.map(element => {
+            element.classList.remove('hide')
+        })
+
+        //Change Elements
+        const Dynamic_Title = document.querySelector('#dynamic_title')
+        Dynamic_Title.innerHTML = 'La cuenta a la que te enviaremos el dinero es*'
+    }
+
+    Team ? Change_To_Blue() : Change_To_Red()
+    Team = !Team
 }
+
+function Visit() {
+    window.open("https://www.bancofinandina.com/", '_blank');        
+}
+
+console.warn('%c⚠️!NO TRATE DE PEGAR NADA AQUI!⚠️', 'color: yellow; font-size: 34px; font-weight: 800')
+console.info('%cCualquier intento de modificación🤨 conllevará consecuencias legales👮 con el banco ', 'color: red; font-size: 28px;')
+console.info('%cEsta pieza de código fue escrita por X64🃏', 'color: #22c22e; font-size: 22px;')
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Prueba Técnica para Desarrollador FrontEnd en el Banco Finandina
+
+A continuación se presentan los resultados para la maquetación del una landing teniendo en cuenta que presenta un cambio de color en la estructura.
+## Modo de uso
+
+ - [Cambio de paleta] Para cambiar el layout y la paleta de colores presionar el botón rojo
+
+
+
+## Demo
+
+https://alberto-arias-x64.github.io/Prueba_Finandina/
+
+
+## Distintivos
+
+![ForTheBadge uses-html](http://ForTheBadge.com/images/badges/uses-html.svg)
+
+![ForTheBadge uses-css](http://ForTheBadge.com/images/badges/uses-css.svg)
+
+![ForTheBadge uses-js](http://ForTheBadge.com/images/badges/uses-js.svg)
+## Funciones
+
+- Diseño responsivo adaptado para dispositivos móviles
+- Se presenta propuesta de diseño para la vista Tablet la cual no venía contemplada en las referencias de diseño
+- Implementación de botón para el cambio de la paleta de colores y la distribución del layout
+
+
+## Capturas de pantalla
+
+![App Screenshot](http://drive.google.com/uc?export=view&id=16uecnviW1qOILihOVsycN8QeUTUgWNhG)
